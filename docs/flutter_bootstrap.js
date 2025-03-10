09588b7c1b5e3463c2063b2133da5e2d03739e7c
Arkar <arkarmin.dev@gmail.com>
2025-03-10 16:29:45 +0700

    deploy bottom nav bar feature

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "107747558"
+    serviceWorkerVersion: "4226659406"
   }
 });
